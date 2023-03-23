792f60ea83746d1ab7bd51a5d2ffc72f0a458b93
Greyfrut1 <vladchumachenko496@gmail.com>
2023-03-23 15:00:46 +0000

    Update

---
+++ web/themes/mytheme/js/script.js
@@ -0,0 +1,21 @@
+(function ($) {
+  $(document).ready(function() {
+    var elements = document.querySelectorAll(".views-field-field-apr-teamlead-1 .form-select  > *:not(:nth-child(3))");
+    var elements2 = document.querySelectorAll(".views-field-field-list-1 .form-select  > *:not(:nth-child(4))");
+    var elements3 = document.querySelectorAll(".views-field-field-apr-teamlead-1 .form-submit");
+    var elements4 = document.querySelectorAll(".views-field-field-list-1 .form-submit");
+    
+    elements.forEach(function(element) {
+        element.parentNode.removeChild(element);
+    });
+    elements2.forEach(function(element) {
+        element.parentNode.removeChild(element);
+    });
+    elements3.forEach(function(element) {
+        element.setAttribute("value", "Approve");
+    });
+    elements4.forEach(function(element) {
+        element.setAttribute("value", "Cancel");
+    });
+  });
+})(jQuery);
